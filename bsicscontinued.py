5efe33383dacc2441df9b28fe8641bb515e9d9b5
Aradhya Kumar <100125236+Aradhya0@users.noreply.github.com>
2023-03-27 14:58:52 +0530

    some more bsics of python

---
+++ bsicscontinued.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range (65,70):
+    # inner loop
+    for j in range(65,i+1):
+        print(chr(j),end="")
+    print()
+
+
+# In[3]:
+
+
+for i in range (65,70):
+    # inner loop
+    for j in range(65,70):
+        print(chr(i),end="")
+    print()
+
+
+# In[11]:
+
+
+for i in range (70,65):
+    # inner loop
+    for j in range(70,65):
+        print(chr(i),end="")
+    print()
+
+
+# In[10]:
+
+
+for i in range (70,65):
+    # inner loop
+    for j in range(70,65):
+        print(chr(i),end="")
+    print()
+
+
+# In[12]:
+
+
+for i in range (65,70):
+    # inner loop
+    for j in range(65,70):
+        print(chr(i),end="")
+    print()
+
+
+# In[17]:
+
+
+for i in range (70,65,-1):
+    # inner loop
+    for j in range(70,65,-1):
+        print(chr(i-1),end="")
+    print()
+
+
+# In[35]:
+
+
+for i in range(0,5):
+    for j in range(0,i+1):
+        print("*",end="")
+    print()
+
+
+# print("aradhya")
+
+# In[38]:
+
+
+for i in range(0,6):
+    for j in range(0,i):
+        print((i),end="")
+    print()
+
+
+# In[39]:
+
+
+for i in range (65,70):
+    # inner loop
+    for j in range(65,i+1):
+        print(chr(j),end="")
+    print()
+
+
+# In[40]:
+
+
+for i in range(0,6):
+    for j in range(0,i):
+        print((j+1),end="")
+    print()
+
+
+# In[114]:
+
+
+for i in range (65,70):
+    # inner loop
+    for j in range(65,i+1):
+        print(chr(i),end="")
+    print()
+
+
+# In[44]:
+
+
+s="PRABHU"
+for i in range (0,len(s)):
+    for j in range(0,i+1):
+        print(s[j],end="")
+    print() 
+
+
+# In[57]:
+
+
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print("*",end="")
+    print()
+
+
+# In[70]:
+
+
+for i in range(1,6):
+    for j in range(i,6):
+        print((i),end="")
+    print()
+
+
+# In[73]:
+
+
+for i in range(6,1,-1):
+    for j in range(1,i,+1):
+        print(j,end="")
+    print()
+
+
+# In[84]:
+
+
+for i in range (65,70):
+    # inner loop
+    for j in range(65,70,+1):
+        print(chr(i),end="")
+    print()
+
+
+# In[91]:
+
+
+for i in range(4):
+
+    for j in range(4):
+
+        print("*", end=' ')
+
+    print("\r")
+
+
+# In[97]:
+
+
+for i in range(65,70):
+    # inner loop
+    for j in range(65,i+1):
+        print(chr(j),end="")
+    print()
+
+
+# In[119]:
+
+
+for i in range (65,70):
+    # inner loop
+    for j in range(i,70):
+        print(chr(i),end="")
+    print()
+
+
+# In[122]:
+
+
+for i in range (65,70):
+    # inner loop
+    for j in range(i,70):
+        print(chr(j),end="")
+    print()
+
+
+# In[ ]:
+
+
+
+
